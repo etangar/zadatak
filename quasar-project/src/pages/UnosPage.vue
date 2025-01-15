9b4da685c16de33fd262db0db87dee41d8b51707
etangar <etangar@veleri.hr>
2025-01-15 10:08:50 +0100

    ispit

---
+++ quasar-project/src/pages/UnosPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <q-page class="flex flex-center">
+        <div><q-banner class="bg-primary text-white" style="max-width: 250px"></q-banner></div>
+    </q-page>
+  </template>
+  
+  <script setup>
+import { QBanner } from 'quasar';
+
+  defineOptions({
+    name: 'IndexPage'
+  });
+  </script>
+  
